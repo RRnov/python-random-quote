c4be87bec0e8427046757634aadbfcdc7b9f42ca
Ruslan <80430448+RRnov@users.noreply.github.com>
2021-04-29 21:40:36 +0300

    First Python program

--- get-quote.py
+++ get-quote.py
@@ -2,14 +2,19 @@ import random
 
 def captain():
   #print("Keep it logically awesome.")
+  f = open("quotes.txt", "a")
+  f.write("Woah there boyo, you added me??\n")
+  f.close()
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  last = 13
+  last = len(quotes)-1
   rnd = random.randint(0, last)
-  print(quotes[rnd])
+  rnd2 = random.randint(0, last)
+  print(quotes[rnd], end='')
+  print(quotes[rnd2], end='')
 
 if __name__== "__main__":
   captain()
